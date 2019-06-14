dba7019d92ecd3ea7390ae4797086977a4cc2f07
sdim-ua <ingersol@yandex.ua>
2019-06-14 10:43:46 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+/* 
+Цепочка наследования
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
